c523b501b2c5cc94f6c23d83274c2b4813b8fed5
Koras02 <wjdgh0727@gmail.com>
2021-08-03 13:17:47 +0900

    About 페이지 완료

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,17 +1,18 @@
 <template>
     <div>
         <hero></hero>
-  
+        <about></about>
     </div>
 </template>
 <script>
 import Hero from "./components/Hero.vue";
- 
+import About from "./components/About.vue"; 
+
 export default {
   name: "components",
   components: {
     Hero,
- 
+    About,
   }
 };
 </script>
